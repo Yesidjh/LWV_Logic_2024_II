ca4c48900255c8af74dcc224dff524e466f722b2
Yesid Murillo <yesid.mjh@gmail.com>
2024-09-03 23:55:31 -0500

    Guardamos avances de mi codigo

--- Ejercicio 1_Coordenadas/Program.cs
+++ Ejercicio 1_Coordenadas/Program.cs
@@ -1,7 +1,17 @@
-﻿internal class Program
+﻿public class Program
 {
+    //Lea las coordenadas de dos puntos y que muestre la distancia entre los dos puntos.
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        //Lea las coordenadas de dospuntos y que muestre las distancias entre los dos puntos
+
+        //Datos de entrada
+        //Declarar las variables, en este caos son 4:
+        double x1 = 4, x2 = 5, y1 = 5.2, y2 = 15.58, d;
+
+        //Vamos a inicilizar mis variables pidiendo valores en pantalla:
+        Console.WriteLine("Por favor ingrese el valor de x1: ");
+        x1 = Convert.ToDouble(Console.ReadLine());
+       
     }
 }
