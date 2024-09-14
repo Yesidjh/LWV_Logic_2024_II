29b752c72e06cc8b1feee772324ee844e16723d7
Yesid Murillo <yesid.mjh@gmail.com>
2024-09-13 20:58:29 -0500

    Parcial 1 ejercicio 1

--- Parcial 1 ejercicio 1/Program.cs
+++ Parcial 1 ejercicio 1/Program.cs
@@ -1,10 +1,12 @@
 ﻿internal class Program
 
 {
+    private static double sueldoBase;
+
     private static void Main(string[] args)
 
 
-   {
+    {
 
 
         //Debemos calcular el dinero que gana el vendedor en base a las comisones por venta, la venta que le djo mayor comision, el promedio de comisones por venta  y si gano o no el beneficio.
@@ -13,7 +15,26 @@
 
         //Proceso:Calcular dinero del vendedor por comisiones tomando su sueldo base mas comisiones, la venta que le genero mayor comsion y el promedio de cimisiones por cada venta.
 
-        Console.WriteLine("Ingrese el sueldo base del vendedor");
+        //Declaracion de variables:
+        double sueldoBase;
+        double venta1, venta2, venta3;
+        double comision1, comision2, comision3;
+
+
+        
+       
+
+
+        Console.WriteLine("Ingrese slario base del vendedor");
+        sueldoBase = Convert.ToDouble(Console.ReadLine());
+        decimal sueldoBase = 2000000;
+
+        Console.WriteLine("Ingrese valor de la primera venta");
+
+
+
+
+ 
 
         
 
