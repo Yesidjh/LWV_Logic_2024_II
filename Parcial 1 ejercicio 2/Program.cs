d4784ba5b9ef553a9c6d124971a4e225c1db7955
Yesid Murillo <yesid.mjh@gmail.com>
2024-09-13 21:59:38 -0500

    parcial 1 ejercicio 2

---
+++ Parcial 1 ejercicio 2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+
+public class Program
+
+{
+    private static double sueldoBase;
+
+    static void Main()
+    { 
+
+
+ //Debemos resolver una ecuacion de primer grado 
+
+//Datos de entrada: Son los 3 coeficientes conque se resolvera la ecuacion
+
+//Proceso:Calcular de la ecuacion de primer grado segun los coeficientes
+
+//Declaracion de variables:
+double 
