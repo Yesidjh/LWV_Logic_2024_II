c31434fa703714b942b8f344baca7525bff8e34c
Yesid Murillo <yesid.mjh@gmail.com>
2024-09-13 22:05:03 -0500

    parcial 1 ejercicio 2

--- Parcial 1 ejercicio 2/Program.cs
+++ Parcial 1 ejercicio 2/Program.cs
@@ -6,14 +6,29 @@ public class Program
     private static double sueldoBase;
 
     static void Main()
-    { 
+    {
 
 
- //Debemos resolver una ecuacion de primer grado 
+        //Debemos resolver una ecuacion de primer grado 
 
-//Datos de entrada: Son los 3 coeficientes conque se resolvera la ecuacion
+        //Datos de entrada: Son los 3 coeficientes conque se resolvera la ecuacion
 
-//Proceso:Calcular de la ecuacion de primer grado segun los coeficientes
+        //Proceso:Calcular de la ecuacion de primer grado segun los coeficientes
 
-//Declaracion de variables:
-double 
+        //Declaracion de variables:
+        double a, b;
+
+        //Ingresar valores de los coeficientes
+        Console.Write("Ingresar coeficiente a: ");
+        a = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Ingresar coeficiente b: ");
+        b = Convert.ToDouble(Console.ReadLine());
+
+        //Calcular y mostrar la solucion.
+        if (a == 0)
+        { 
+            if (b == 0)
+
+
+
