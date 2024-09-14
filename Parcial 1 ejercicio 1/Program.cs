c2799c3f42e322212c18b88e829aae6ef314e3ac
Yesid Murillo <yesid.mjh@gmail.com>
2024-09-13 20:42:27 -0500

    Ejercicio 1 Parcial 1

---
+++ Parcial 1 ejercicio 1/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+
+{
+    private static void Main(string[] args)
+
+
+   {
+
+
+        //Debemos calcular el dinero que gana el vendedor en base a las comisones por venta, la venta que le djo mayor comision, el promedio de comisones por venta  y si gano o no el beneficio.
+
+        //Datos de entrada: Salario base, comision por ventas y beneficio extra por cumpliento de obetivo del mes.
+
+        //Proceso:Calcular dinero del vendedor por comisiones tomando su sueldo base mas comisiones, la venta que le genero mayor comsion y el promedio de cimisiones por cada venta.
+
+        Console.WriteLine("Ingrese el sueldo base del vendedor");
+
+        
+
+    }
+}
