8f3e15dc22613f3037080933f2899505598e2b06
Yesid Murillo <yesid.mjh@gmail.com>
2024-10-08 18:16:07 -0500

    Ejercicio 2 parcial 2

---
+++ Parcial 2 ejercicio 2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+
+public class Program
+
+{ 
+
+    private static void Main(string[] args)
+
+    {
+
+        //ingresar las notas de varios estudiantes, calcular su promedio y luego clasificar a cada estudiante en diferentes categorías de desempeño académico. Al final, se le pregunta al usuario si quiere ingresar otro grupo de estudiantes
+
+        do
+
+        {
+            List<double> notas = new List<double>();
+            string continuar;
+
+            Console.Write("Porfavor ingresa la cantidad de estudinates");
+            int cantidadEstudiantes = int.Parse(Console.ReadLine());
+
+            fot
+
+
+        
