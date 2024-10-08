cfabdc775be76d37a0dd72da74f330ad8f791cee
Yesid Murillo <yesid.mjh@gmail.com>
2024-10-07 20:27:02 -0500

    Parcial 2 ejercicio 1

---
+++ Parcial 2 ejercicio 1/Program.cs
@@ -0,0 +1,19 @@
+﻿using static System.Formats.Asn1.AsnWriter;
+
+public class Program
+{ 
+    private static void Main(string[] args)
+    {
+        //Adivina el número. Crear un programa que consiste en adivinar un número N aleatorio que el sistema escoge de un determinado rango y lo guarda enmemoria sin ser revelado.
+
+        //El juego debe tener la funcionalidad de escoger el número de participantes que jugarán: mínimo 2 y máximo 4 integrantes.Dependiendo de la cantidad de jugadores, el número ℕ aleatorio se generar en los siguientes rangos:
+        
+        //Si participan 2 jugadores, el número ℕ aleatorio se generará entre 0 y 50
+    
+       //Si participan 3 jugadores, el número ℕ aleatorio se generará entre 0 y 100 
+
+      //Si participan 4 jugadores, el número ℕ aleatorio se generará entre 0 y 200 
+
+
+
+
