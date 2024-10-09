977ba4f12cfae6d0aa99ed056fa6a4b9d45f7ef9
Yesid Murillo <yesid.mjh@gmail.com>
2024-10-08 19:01:02 -0500

    Parcial 2 ejercicio 1

--- Parcial 2 ejercicio 1/Program.cs
+++ Parcial 2 ejercicio 1/Program.cs
@@ -1,14 +1,11 @@
-﻿using System.ComponentModel.Design;
-using System.Reflection;
-using static System.Formats.Asn1.AsnWriter;
+﻿
 
 public class Program
 {
     
-    private static int numeroAleatorio;
-    private static bool fasle;
 
     private static void Main(string[] args)
+
     {
         //Adivina el número. Crear un programa que consiste en adivinar un número N aleatorio que el sistema escoge de un determinado rango y lo guarda enmemoria sin ser revelado.
 
@@ -23,115 +20,12 @@ public class Program
         //Numero de jugadores
 
 
-        bool jugarnuevamente;
+        Random random = new Random();
+        bool jugarOtraVez;
 
         do
-
         {
-            jugarnuevamente = false;
-            int jugadores = 0;
-
-
-            //Seleccionar jugador
-
-            while (jugadores < 2 || jugadores > 4)
-            {
-                Console.WriteLine("Porfavor ingrese el numero de jugadores (entre 2 y 4 jugadotes):");
-                if (int.TryParse(Console.ReadLine(), out jugadores) && jugadores >= 2 && jugadores <= 4)
-
-                {
-                    break;
-                }
-                Console.WriteLine("Numero invalido. Intente nuvamente");
-
-            }
-
-            //Generar numero aleatorio segun nuemro de jugadores
-
-            int numeroAleaotorio = NumeroAleatorio(jugadores);
-
-            Console.WriteLine($"Se genero un numero entre 0 y {GetMaximo(jugadores)}.");
-
-            int jugadorActual = 0;
-            bool ganador = false;
-
-            while (!ganador)
-            {
-                Console.WriteLine($"{jugadorActual + 1}, ingresa un numero:");
-                int intento;
-                if (int.TryParse(Console.ReadLine(), out intento))
-                {
-                    if (intento > numeroAleatorio)
-                    {
-                        Console.WriteLine("Menor");
-                    }
-                    else if (intento > numeroAleatorio)
-                    {
-                        Console.WriteLine("Mayor");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Has ganado");
-                        ganador = true;
-                    }
-
-                    //Cambio al siguiente jugador
-                    jugadorActual = (jugadorActual + 1) % jugadores;
-
-                }
-                else
-                {
-
-                    Console.WriteLine("Numero invalido. Intente nuvamente");
-                }
-
-            }
-
-
-            //Preguntar si quiere jugar nuevamente
-            Console.WriteLine("¿Quieren jugar de nuevo? (s/n)");
-
-
-            if (Console.ReadLine().ToLower() == "s")
-
-            {
-                jugarnuevamente = true;
-
-                //Limpiar consola para nueve partida
-
-                Console.Clear();
-            }
-
-        } while (jugarnuevamente);
-
-    }
-
-    static int GenerarNumeroAleatorio(int jugadores)
-    {
-
-    }
-
-    static int GenerarNumeroAleatorio(int jugadores)
-    {
-        Random random = new Random();
-        return random.Next(0, GetMaximo(jugadores) + 1);
-    }
-
-    static int GetMaximo(int jugadores)
-    {
-        switch (jugadores)
-        {
-            case 2:
-                return 50;
-            case3:
-                return 100;
-            case 4:
-                return 200;
-            default:
-                return 0;
-        }
-    }
-}
+            int cantidadJugadores;
 
 
 
